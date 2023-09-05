02bb4044455ee663199efccc831519d9435af909
Miko <142531709+MikoInSpace@users.noreply.github.com>
2023-09-05 20:14:38 +0200

    Added Canadian Dollars
    Added CAD $
    Fixed some code.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 
-# Function to calculate total money in Euros
 def calceuro():
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("Euro Money Calculator")
     print("Enter the number of each Euro bill/coin denomination:")
     
     # Input the number of each Euro bill/coin denomination
-    os.system('cls' if os.name == 'nt' else 'clear')    
     one = int(input("Number of €1 coins: "))
     os.system('cls' if os.name == 'nt' else 'clear')
     two = int(input("Number of €2 coins: "))
@@ -34,13 +33,12 @@ def calceuro():
     # Display the total amount in Euros
     print(f"\nTotal amount in Euros: €{total_euro:.2f}")
 
-
 def calcdollar():
+    os.system('cls' if os.name == 'nt' else 'clear')
     print("Dollar Money Calculator")
     print("Enter the number of each Dollar Bill denomination:")
     
     # Input the number of each Dollar bill denomination
-    os.system('cls' if os.name == 'nt' else 'clear')    
     one = int(input("Number of $1 bills: "))
     os.system('cls' if os.name == 'nt' else 'clear')
     two = int(input("Number of $2 bills: "))
@@ -63,35 +61,70 @@ def calcdollar():
     # Display the total amount in Dollars
     print(f"\nTotal amount in Dollars: ${total_dollar:.2f}")
 
+def calccaddollar():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("Canadian Dollar Money Calculator")
+    print("Enter the number of each Canadian Dollar Bill/Coin denomination:")
+    
+    # Input the number of each Canadian Dollar coin denomination
+    nickel = int(input("Number of 5 cent coins: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    dime = int(input("Number of 10 cent coins: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    quarter = int(input("Number of 25 cent coins: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    fifty_cent = int(input("Number of 50 cent coins: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    one = int(input("Number of $1 coins: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    two = int(input("Number of $2 coins: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    five = int(input("Number of $5 bills: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    ten = int(input("Number of $10 bills: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    twenty = int(input("Number of $20 bills: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    fifty = int(input("Number of $50 bills: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    hundred = int(input("Number of $100 bills: "))
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    # Calculate the total amount in Canadian Dollars
+    total_dollar = (nickel * 0.05) + (dime * 0.10) + (quarter * 0.25) + (fifty_cent * 0.50) + \
+                   (one * 1) + (two * 2) + (five * 5) + (ten * 10) + \
+                   (twenty * 20) + (fifty * 50) + (hundred * 100)
+    
+    # Display the total amount in Canadian Dollars
+    print(f"\nTotal amount in Canadian Dollars: ${total_dollar:.2f}")
+
 
 while True:
+    print("1. Choose currency.")
+    print("2. Exit.")
+    
+    selection = input("Selection: ")
 
- print("1. Choose currency.")
- print("2. Exit.")
- 
- selection = input("Selection: ")
+    if selection == "1":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("1. Euro")
+        print("2. Dollar")
+        print("3. Canadian Dollar")
 
- if selection == "1":
-     os.system('cls' if os.name == 'nt' else 'clear')
-     print("1. Euro")
-     print("2. Dollar")
+        selection = input("Selection: ")
 
-     selection = input("Selection: ")
-
-     if selection == "1":
-         calceuro()
-     elif selection == "2":
-         calcdollar()
-     else:
-         print("Invalid selection.")
-
- elif selection == "2":
-     os.system('cls' if os.name == 'nt' else 'clear')
-     break
- 
- else:
-     os.system('cls' if os.name == 'nt' else 'clear')
-     print("Invalid selection.")
- 
-
-
+        if selection == "1":
+            calceuro()
+        elif selection == "2":
+            calcdollar()
+        elif selection == "3":
+            calccaddollar()
+        else:
+            print("Invalid selection.")
+    
+    elif selection == "2":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        break
+    
+    else:
+        print("Invalid selection.")
